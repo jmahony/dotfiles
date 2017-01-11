69deaba13dce347675ed1cebb3dd2fe56efd619a
Josh Mahony <manny@masterofmalt.com>
2017-01-11 13:55:22 +0000

    Sync

---
+++ windows/scripts/BackUpJumpLocation.ps1
@@ -0,0 +1,7 @@
+﻿$versionDirectory = "$Env:USERPROFILE\jump_location_backups"
+
+if (-Not (Test-Path -Path $versionDirectory)) {
+    New-Item -ItemType directory -Path $versionDirectory
+}
+
+Copy-Item "$Env:USERPROFILE\jump-location.txt" "$versionDirectory\jump-location-$(get-date -f yyyyMMddhhmmss).txt"
