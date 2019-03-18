6b23ddecee48d2e465acc66d67c31abba64610d1
Josh Mahony <josh mahony@atom2.local>
2019-03-18 13:46:30 +0000

    Sync

--- windows/power_shell/prompt.ps1
+++ windows/power_shell/prompt.ps1
@@ -2,24 +2,11 @@ function prompt() {
     $temp = Get-GitStatus
 
     if($temp -ne $null) {
-        $windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-        $windowsPrincipal = new-object 'System.Security.Principal.WindowsPrincipal' $windowsIdentity
-
-        if ($psISE) {
-            $color = "Black";
-        } elseif ($windowsPrincipal.IsInRole("Administrators") -eq 1) {
-            $color = "Red";
-        }
-        else {
-            $color = "Green";
-        }
-
-        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
         Write-VcsStatus
         write-host " " -NoNewLine
         Write-Host( $pwd.ProviderPath.replace($temp.GitDir.replace(".git", ""), "") ) -nonewline
         write-host " " -NoNewLine
-        write-host "$" -NoNewLine -ForegroundColor $color
+        write-host "$" -NoNewLine -ForegroundColor Green
 
         return " "
     } else {
